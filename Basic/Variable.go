014ee0b29accd8ad72e75f412315816432b0b2e2
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-02-22 01:20:04 +0530

    Added Variable Files

---
+++ Basic/Variable.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrintVariables() {
+
+	var name string = "Jalpesh Vadgama"
+	fmt.Println(name)
+
+	var a, b int = 1, 2
+	fmt.Println(a, b)
+
+	var d = true
+	fmt.Println(d)
+
+}
