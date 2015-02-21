6772aa45741a2a2d938b07533a5b70f40f0d3514
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-02-22 01:28:41 +0530

    Added Costant

---
+++ Basic/Costant.go
@@ -0,0 +1,13 @@
+// Package main provides ...
+package main
+import (
+	"fmt",
+	"math"
+)
+func PrintCostant() {
+	const nameCosntant  string ="Jalpesh Vadgama"
+	fmt.PrintLn(nameCosntant)
+	
+	const pi int = 22 / 7
+	fmt.PrintLn(pi)
+	}
